290aaf16d08bec6d692b8968386992f19c8603dc
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2020-08-04 11:59:40 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -3,10 +3,20 @@ package gui.util;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
+import model.entities.Department;
 
 public class Utils {
 	
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}	
 }
